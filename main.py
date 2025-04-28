a6e34e7d3d3a65d4528aed5b8a410e3ab1020472
Rogov-Anton <anton.rogov.1210@mail.ru>
2025-04-28 16:45:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,174 +1,92 @@
-WHITE, BLACK = 1, 2
-
-
-# Функция для вычисления цвета противника
-def opponent(color):
-    return WHITE if color == BLACK else BLACK
-
-
-def print_board(board):  # Распечатать доску в текстовом виде
-    print('     +----+----+----+----+----+----+----+----+')
-    for row in range(7, -1, -1):
-        print(' ', row, end='  ')
-        for col in range(8):
-            print('|', board.cell(row, col), end=' ')
-        print('|')
-        print('     +----+----+----+----+----+----+----+----+')
-    print(end='        ')
-    for col in range(8):
-        print(col, end='    ')
-    print()
-
-
-def coords_in_field(row, col):
-    '''Функция проверяет, что координаты (row, col) лежат
-    внутри доски'''
-    return 0 <= row < 8 and 0 <= col < 8
-
-
-class Board:
+class FileSystem:
     def __init__(self):
-        self.turn = WHITE
-        self.field = [[None for i in range(8)] for j in range(8)]
-        # Пешка белого цвета в клетке E2.
-        # self.field[1][4] = Pawn(1, 4, WHITE)
+        self.files = [
+            Directory('1'),
+            Directory('2')
+        ]  # корневой каталог
 
-    def cell(self, row, col):
-        '''Возвращает строку из двух символов. Если в клетке (row, col)
-        находится фигура, символы цвета и фигуры. Если клетка пуста,
-        то два пробела.'''
-        piece = self.field[row][col]
-        if piece is None:
-            return '  '
-        color = piece.get_color()
-        c = 'w' if color == WHITE else 'b'
-        return c + piece.char()
+    def make_directory(self, name: str, path: str):
+        if path == '/':  # Если путь не указан, то создать папку в корневом каталоге
+            self.files.append(Directory(name))
+            return True
 
-    def move_piece(self, row, col, row1, col1):
-        '''Переместить фигуру из точки (row, col) в точку (row1, col1).
-        Если перемещение возможно, метод выполнит его и вернет True.
-        Если нет --- вернет False'''
+        parts = path.split('/')
+        current_dir = self  # текущая дериктория
+        idx = 0
 
-        if not coords_in_field(row, col) or not coords_in_field(row1, col1):
-            return False
-        if row == row1 and col == col1:
-            return False  # нельзя пойти в ту же клетку
-        piece = self.field[row][col]
-        if piece is None:
-            return False
-        if piece.color != self.turn:
-            return False
-        if not piece.can_move(row1, col1):
-            return False
-        self.field[row][col] = None  # Снять фигуру.
-        self.field[row1][col1] = piece  # Поставить на новое место.
-        piece.set_position(row1, col1)
-        self.turn = opponent(self.turn)
+        while len(parts) - 2 >= idx:
+            for elem in current_dir.files:  # Проходим по всем файлам в текущей директории
+                if isinstance(elem, Directory):  # Если элемент - это дирректория
+                    if elem.name == parts[idx]:
+                        current_dir = elem
+                        idx += 1
+                        break
+            else:  # Если дирректория не найдена, то выдать сообщение об ошибке
+                return False
+
+        current_dir.files.append(Directory(name))
+
+    def list_directory(self, path):
+        if path == '/':
+            return self.files
+
+        parts = path.split('/')
+        current_dir = self  # текущая дериктория
+        idx = 0
+
+        while len(parts) - 2 >= idx:
+            for elem in current_dir.files:  # Проходим по всем файлам в текущей директории
+                if isinstance(elem, Directory):  # Если элемент - это дирректория
+                    if elem.name == parts[idx]:
+                        current_dir = elem
+                        idx += 1
+                        break
+            else:  # Если дирректория не найдена, то выдать сообщение об ошибке
+                return False
+
+        return current_dir.files
+
+
+class Directory:
+    def __init__(self, name):
+        self.name = name
+        self.files = []
+
+    def __repr__(self):
+        BLUE = '\033[34m'
+        RESET = '\033[0m'
+        print(f"{BLUE}{self.name}{RESET}")
+
+
+def print_quide():
+    print('<exit> - quit')
+    print('<mkdir> {path} - make directory')
+    print('')
+
+
+def execute_command(filesystem: FileSystem, text):  # Функция для выполнения команд
+    if text == 'man':
+        print_quide()
+        return True
+    parts = text.split()
+    command = parts[0]
+    if command == 'mkdir':
+        filesystem.make_directory(parts[1], parts[2])
+        return True
+    if command == 'ls':
+        result = filesystem.list_directory(parts[1])
+        print(result)
         return True
 
 
-class Figure:  # Базовый класс для фигур
-    def __init__(self, row, col, color):
-        self.row = row  # текущий ряд
-        self.col = col  # текущий столбец
-        self.color = color
+print('This is my file-system. Type <man> to get guide')
+filesystem = FileSystem()
+command = input()
 
-    def set_position(self, row, col):
-        self.row = row
-        self.col = col
+execute_command(filesystem, command)
 
-    def get_color(self):
-        return self.color
-
-
-class Pawn(Figure):
-    def char(self):
-        return 'P'
-
-    def can_move(self, row, col):
-        # Пешка может ходить только по вертикали
-        # "взятие на проходе" не реализовано
-        if self.col != col:
-            return False
-
-        # Пешка может сделать из начального положения ход на 2 клетки
-        # вперёд, поэтому поместим индекс начального ряда в start_row.
-        if self.color == WHITE:
-            direction = 1
-            start_row = 1
-        else:
-            direction = -1
-            start_row = 6
-
-        # ход на 1 клетку
-        if self.row + direction == row:
-            return True
-
-        # ход на 2 клетки из начального положения
-        if self.row == start_row and self.row + 2 * direction == row:
-            return True
-
-        return False
-
-
-class Rook(Figure):
-    def char(self):
-        return 'R'
-
-    def can_move(self, row, col):
-        '''Невозможно сделать ход в клетку, которая не лежит в том же ряду
-        или столбце клеток.'''
-        if self.row != row and self.col != col:
-            return False
-        return True
-
-
-class Bishop(Figure):  # Слон
-    def char(self):
-        return 'B'
-
-    def can_move(self, row, col):
-        if self.row - self.col == row - col:
-            return True
-        if self.row + self.col == row + col:
-            return True
-        return False
-
-
-def main():
-    # Создаём шахматную доску
-    board = Board()
-    # Цикл ввода команд игроков
-    while True:
-        # Выводим положение фигур на доске
-        print_board(board)
-        # Подсказка по командам
-        print('Команды:')
-        print('    exit                               -- выход')
-        print('    move <row> <col> <row1> <col1>     -- ход из клетки (row, col)')
-        print('                                          в клетку (row1, col1)')
-        # Выводим приглашение игроку нужного цвета
-        if board.turn == WHITE:
-            print('Ход белых:')
-        else:
-            print('Ход черных:')
-        command = input()
-        if command == 'exit':
-            break
-        move_type, row, col, row1, col1 = command.split()
-        row, col, row1, col1 = int(row), int(col), int(row1), int(col1)
-        if board.move_piece(row, col, row1, col1):
-            print('Ход успешен')
-        else:
-            print('Координаты некорректы! Попробуйте другой ход!')
-
-
-if __name__ == '__main__':
-    f = Board()
-    b = Bishop(4, 2, BLACK)
-    f.field[4][2] = b
-    for i in range(8):
-        for j in range(8):
-            if b.can_move(i, j):
-                f.field[i][j] = b
-    print_board(f)
+"""
+while command != 'exit':
+    execute_command(filesystem, command)
+    command = input()
+"""
